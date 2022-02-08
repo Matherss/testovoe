cc046e8694aed2d5773cc2e613c264d2e18a42a2
Matherss <mathers_beats@mail.ru>
2022-02-08 18:09:22 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,147 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+    <input type="text" v-model="searchText" placeholder="Название команды" />
+    <div class="container">
+      <card
+        v-for="(item, index) of filters"
+        :key="index"
+        :teamHome="item.teamHome.name"
+        :teamHomeShortName="item.teamHome.shortName"
+        :teamHomeLogo="item.teamHome.logo"
+        :teamHomeLogoId="item.teamHome.logoId"
+        :teamAway="item.teamAway.name"
+        :teamAwayShortName="item.teamAway.shortName"
+        :teamAwayLogo="item.teamAway.logo"
+        :scoreFtHome="item.scoreFtHome"
+        :scoreFtAway="item.scoreFtAway"
+        :_id="item._id"
+        :tourNumber="item.tourNumber"
+        :stadium="item.stadium"
+        :date="item.date"
+      >
+      </card>
+    </div>
+    <button @click="update()">Загрузить ещё</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import Card from "./components/Card.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Card
   },
+  data() {
+    return {
+      info: null,
+      limit: 0,
+      searchText: ""
+    };
+  },
+  beforeMount() {
+    this.update();
+  },
+  methods: {
+    update() {
+      this.limit = this.limit + 10;
+      axios({
+        url: "https://footballista.ru/graphql",
+        method: "post",
+        data: {
+          query: `{
+ Season(_id: 5099) {
+  calendar(offset:0, limit:${this.limit}) {
+    total, items {
+      _id
+      tourNumber
+      teamHome {name, shortName, logo, logoId }
+      teamAway {name, shortName, logo, logoId }
+      scoreFtHome
+      scoreFtAway
+      stadium { name }
+      date
+    }
+  }
+ }}`
+        }
+      }).then((result) => {
+        this.info = result.data;
+        console.log(this.info);
+      });
+    }
+  },
+  computed: {
+    filters() {
+      let regexp = new RegExp(this.searchText, "i");
+      return this.info.data.Season.calendar.items.filter(
+        (item) => item.teamHome.name.match(regexp) || item.teamAway.name.match(regexp)
+      );
+    }
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+.wrapper {
+  max-width: 1230px;
+  margin: 0 auto;
+}
+.container {
+  max-width: 1230px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+  grid-gap: 2px;
+}
+@media screen and (max-width: 767px) {
+  .container {
+    grid-template-columns: 1fr;
+  }
+}
+
+button {
+  background-color: initial;
+  background-image: linear-gradient(#8614f8 0, #760be0 100%);
+  border-radius: 5px;
+  border-style: none;
+  box-shadow: rgba(245, 244, 247, 0.25) 0 1px 1px inset;
+  color: #fff;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  font-family: Inter, sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  height: 60px;
+  line-height: 60px;
+  margin-left: 15px;
+  outline: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  transition: all 0.3s cubic-bezier(0.05, 0.03, 0.35, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: bottom;
+  width: 190px;
+}
+
+button:hover {
+  opacity: 0.7;
+}
+
+@media screen and (max-width: 1000px) {
+  button {
+    font-size: 14px;
+    height: 55px;
+    line-height: 55px;
+    width: 150px;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="card__wrapper">
+    <div class="games">
+      <div class="firstTeam">
+        <img :src="logoHomeSrc" /><span class="firstTeamName">{{ teamHome }} ({{ teamHomeShortName }})</span>
+      </div>
+      <div class="scores">
+        <span class="score">{{ scoreFtHome }}</span
+        >:<span class="score">{{ scoreFtAway }}</span>
+      </div>
+
+      <div class="secondTeam">
+        <span class="secondTeamName">{{ teamAway }} ({{ teamAwayShortName }})</span><img :src="logoAwaySrc" />
+      </div>
+    </div>
+    <div class="matchInfo">
+      <span v-if="stadium !== null">Стадион: {{ stadium.name }},</span> Дата: {{ actualDate }} | Тур: {{ tourNumber }} |
+      ID: {{ _id }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    teamHome: String,
+    teamHomeShortName: String,
+    teamHomeLogo: String,
+    teamHomeLogoId: Number,
+    teamAway: String,
+    teamAwayShortName: String,
+    teamAwayLogo: String,
+    scoreFtHome: Number,
+    scoreFtAway: Number,
+    _id: String,
+    tourNumber: Number,
+    stadium: Object,
+    date: String
+  },
+
+  mounted() {
+    console.log(this.teamHomeLogo);
+    return {};
+  },
+  computed: {
+    logoHomeSrc() {
+      return `https://footballista.ru/api/img/logos/${this.teamHomeLogo}-middle.png?`;
+    },
+    logoAwaySrc() {
+      return `https://footballista.ru/api/img/logos/${this.teamAwayLogo}-middle.png?`;
+    },
+    actualDate() {
+      return this.date.match(/.{10}/).toString();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.matchInfo {
+  padding: 5px 0px 15px;
+}
+.games {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 0;
+}
+.card__wrapper {
+  padding: 5px;
+}
+
+.firstTeam,
+.secondTeam {
+  border: 1px solid black;
+  width: 272px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  text-align: left;
+}
+.firstTeamName,
+.secondTeamName {
+  display: inline-block;
+}
+.scores {
+  margin: 15px;
+  display: flex;
+  align-items: center;
+}
+.score {
+  margin: 2px;
+}
+img {
+  width: 60px;
+  height: 60px;
+}
+</style>
